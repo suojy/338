956853851d47e9fe2fff0c902ecf8dcd48d4eab7
suojy <zhuzhubaobeiok@126.com>
2019-06-05 13:55:00 -0500

    forth

--- flask-backend/utils/twitter_worker.py
+++ flask-backend/utils/twitter_worker.py
@@ -44,29 +44,49 @@ class TwitterWorker(object):
                 break
 
     def get_tweets(self, handle):
-        posts = []
+        # posts = []
+        # if self.validate_handle(handle):
+        #     try:
+        #         i = 0
+        #         cursor = tweepy.Cursor(self.api.user_timeline, screen_name=handle, tweet_mode="extended",
+        #                                include_rts=True).items()
+        #         for status in self._tweet_yielder(cursor):
+        #             i += 1
+        #             try:
+        #                 tweet = status._json['retweeted_status']['full_text']
+        #             except:
+        #                 tweet = status._json['full_text']
+        #             posts.append(tweet)
+        #             if i >= self.max_tweets: break
+        #     except tweepy.TweepError as e:
+        #         return []
+        # # import unicodecsv as csv
+        # # with open('/Users/salvatoregiorgi/Documents/' + handle + '.csv', "wb") as csv_file:
+        # #     writer = csv.writer(csv_file, delimiter=',')
+        # #     for line in posts:
+        # #         writer.writerow([line])
+        # # print("LENGTH", len(posts))
+        # return posts
+        all_tweets = []
+        all_the_tweets = []
         if self.validate_handle(handle):
             try:
-                i = 0
-                cursor = tweepy.Cursor(self.api.user_timeline, screen_name=handle, tweet_mode="extended",
-                                       include_rts=True).items()
-                for status in self._tweet_yielder(cursor):
-                    i += 1
-                    try:
-                        tweet = status._json['retweeted_status']['full_text']
-                    except:
-                        tweet = status._json['full_text']
-                    posts.append(tweet)
-                    if i >= self.max_tweets: break
+                new_tweets = self.api.user_timeline(screen_name=handle, count=100)
+                all_the_tweets.extend(new_tweets)
+
+                outtweets = [[tweet.text] for tweet in all_the_tweets]
+                if len(outtweets) == 0:
+                    return
+
+                for tweet in outtweets:
+                    for actual_tweet in tweet:
+                        string_form = actual_tweet.encode("ascii", errors="ignore").decode()
+                        all_tweets.append(string_form)
             except tweepy.TweepError as e:
                 return []
-        # import unicodecsv as csv
-        # with open('/Users/salvatoregiorgi/Documents/' + handle + '.csv', "wb") as csv_file:
-        #     writer = csv.writer(csv_file, delimiter=',')
-        #     for line in posts:
-        #         writer.writerow([line])
-        # print("LENGTH", len(posts))
-        return posts
+        
+        return all_tweets
+        
 
     def get_profile(self, handle):
         user = self.validate_handle(handle)
